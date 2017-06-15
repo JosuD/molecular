33bf724893fa5e8dfa52e2a19a2cfb007b5991ca
José <liajose0@gmail.com>
2017-06-15 06:51:36 -0300

    Commit inicial.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Dinámica Molecular
+
+Este repositorio contiene el código correspondiente al tercer trabajo práctico de la
+materia [Física Computacional](http://materias.df.uba.ar/compua2017c1/) del primer
+cuatrimestre de 2017.
---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <error.h>
+#include <math.h>
+
+int main (int argc, char *argv[]) {
+
+  int status;
+
+  status = EXIT_SUCCESS;
+
+  return status;
+}
+
