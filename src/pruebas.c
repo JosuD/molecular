68cfdf120cea223155cc165e03b5afe6911a9a3d
German Caro <german.layer8@gmail.com>
2017-06-20 17:22:56 -0300

    Listo potencial y fuerza con tabla

--- src/pruebas.c
+++ src/pruebas.c
@@ -3,11 +3,17 @@
  */
 #include <stdlib.h>
 #include "table.h"
+#include <stdio.h>
 
 int main()
 {
     float **tpot, **tforce;
     int lineas;
-    lineas = load_table(tpot, tforce);
+    lineas = load_table(&tpot, &tforce);
+    printf("%f %f\n", tpot[0][0], tpot[1][0]);
+    double potencial = apppot(tpot, 1.07);
+    double fuerza = appforce(tforce, 1.07);
+
+    printf("potencial %f, fuerza %f\n", potencial, fuerza);
 }
 
