a7f30f1fb7ad04c1bafb9ad74c899732931c0f98
German Caro <german.layer8@gmail.com>
2017-06-20 16:25:21 -0300

    Agregada una función para cargar la tabla
    Estoy usando float's en lugar de double's después de una hora de pelear
    con fscanf. Eso eventualmente se puede cambiar en cuando encuentre como
    hacerlo. El archivo prueba.c lo voy a usar para probar las funciones de
    table.c, nos puede servir de unit test. Quizás le vendría bien un nombre
    más copado ...

---
+++ src/pruebas.c
@@ -0,0 +1,13 @@
+/*
+ * Este es un script de prueba para las funciones de table.c/h
+ */
+#include <stdlib.h>
+#include "table.h"
+
+int main()
+{
+    float **tpot, **tforce;
+    int lineas;
+    lineas = load_table(tpot, tforce);
+}
+
--- src/table.h
+++ src/table.h
@@ -1,3 +1,6 @@
 int gen_tabla();
 double force(double r);
 double potencial(double r);
+int load_table(float **tpot, float **tforce);
+double apppot(float *tpot, double r);
+double appforce(float *tforce, double r);
